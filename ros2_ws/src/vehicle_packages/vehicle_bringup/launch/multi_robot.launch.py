977d76b646d3514cd0b9ddbd730ee2580210f90c
Nadav Cherry <nadavcherry@gmail.com>
2025-07-17 14:49:02 +0300

    fix world

--- ros2_ws/src/vehicle_packages/vehicle_bringup/launch/multi_robot.launch.py
+++ ros2_ws/src/vehicle_packages/vehicle_bringup/launch/multi_robot.launch.py
@@ -55,11 +55,7 @@ def generate_launch_description():
             ),
             launch_arguments={"robot_ns": f"robot_{i}/"}.items(),
         )
-<<<<<<< Updated upstream
-        for i in range(0, 2)
-=======
         for i in range(0, 1)
->>>>>>> Stashed changes
     ]
 
     return LaunchDescription(
